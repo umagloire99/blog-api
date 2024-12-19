16ede8eafc98ff3efabea8913adf29b755ccd7f9
Ulrich Magloire <umagloire99@gmail.com>
2024-12-19 09:49:57 +0100

    +

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,6 +24,14 @@ class AuthController extends Controller
     /**
      * Register user
      *
+     * @unauthenticated
+     * @header Content-Type application/json
+
+     * @bodyParam name string required The last name of the user. Example: John Doe
+     * @bodyParam email string required The email of the User. Example: user@email.com
+     * @bodyParam password string required The password of the User. Example: 123@!PASSWORD
+     * @bodyParam password_confirmation string required The confirmation of the password. Example: 123@!PASSWORD
+     *
      * @param  Request $request
      */
     public function register(Request $request)
@@ -52,6 +60,20 @@ class AuthController extends Controller
 
     /**
      * Login
+     *
+     * @unauthenticated
+     * @header Content-Type application/json
+     *
+     * Handle a login request to the application. Below are the default users with different role generated from the seeder.
+     *
+     * Administrator: admin@blog.com manage all posts, users, comments, roles, and tags.
+     *
+     * Author: author@blog.com manage his own posts, comments and tags.
+     *
+     * User: user@blog.com normal user that can read posts and leave a comment.
+
+     * @bodyParam email string required The email of the User. Example: admin@blog.com
+     * @bodyParam password string required The password of the User. Example: Password.0!
      *
      * @param  Request $request
      */

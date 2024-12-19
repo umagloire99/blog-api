9cc4fef5f7af576d69ece0ba67e4df41feaeb6fd
Ulrich Magloire <umagloire99@gmail.com>
2024-12-19 03:10:58 +0100

    +

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::fallback(function () { // this route will be executed when no other route matches the incoming request.
-    return response()->json([
-        'status' => false,
-        'message' => 'The resource was not found!',
-    ], 404);
-})->name('web.fallback.404');
-
